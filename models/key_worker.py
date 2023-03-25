1d6d9953e198450209a29ef7deb4a01922110e67
Andrew Dyson <axd659@gmail.com>
2023-03-25 10:42:06 +0000

    flask running

---
+++ models/key_worker.py
@@ -0,0 +1,8 @@
+class KeyWorker:
+    def __init__(self, name, room):
+        self.name = name
+        self.room = room
+        self.assigned_children = []
+
+
+        
---
+++ models/room.py
@@ -0,0 +1,7 @@
+class Room:
+    def __init__(self, name, key_worker):
+        self.name = name
+        self.key_worker = key_worker
+        self.children = []
+
+        
